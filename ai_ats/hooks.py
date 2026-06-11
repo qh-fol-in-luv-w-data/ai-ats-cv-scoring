adeea15630584dff3a1ed8798d1e10fcf4a73cb8
Le Thanh Anh <leejay3636@gmail.com>
2026-06-11 15:05:42 +0700

    feat: add www portal pages and update recent changes

--- ai_ats/hooks.py
+++ ai_ats/hooks.py
@@ -7,3 +7,10 @@ app_license = "mit"
 
 # Expose API
 override_whitelisted_methods = {}
+
+
+# SPA Routing
+website_route_rules = [
+    {"from_route": "/aicenter/2as-ats/<path:app_path>", "to_route": "ai_ats_spa"},
+    {"from_route": "/aicenter/2as-ats", "to_route": "ai_ats_spa"}
+]
